59cff90b9747825da3c898f82f02077faba1dd31
mike <apolloproductionsdj@gmail.com>
2021-04-29 21:34:56 -0700

    added firebase config to app

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbVNx1SvksS4J9-9g8klJPzMHglGbWTEQ",
+    authDomain: "signal-clone-e4200.firebaseapp.com",
+    projectId: "signal-clone-e4200",
+    storageBucket: "signal-clone-e4200.appspot.com",
+    messagingSenderId: "920417580678",
+    appId: "1:920417580678:web:9043c3b07dc753ef430307"
+};
