bce657e1c3a497ed7534744ebf524211a7765a30
mike <apolloproductionsdj@gmail.com>
2021-05-14 11:20:41 -0700

    added in expo elements for design and now have a headerRight

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,9 +5,16 @@ import { TouchableOpacity } from 'react-native'
 import { StyleSheet, Text, ScrollView } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
 import CustomListItem from '../components/CustomListItem';
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons';
 import { auth, db } from '../firebase';
 
 const HomeScreen = ({ navigation }) => {
+
+    const signOutUser = () => {
+        auth.signOut().then(() => {
+            navigation.replace('Login')
+        });
+    };
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -17,16 +24,33 @@ const HomeScreen = ({ navigation }) => {
             headerTintColor: 'black',
             headerLeft: () => (
                 <View style={{ marginLeft: 20 }}>
-                    <TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={signOutUser}
+                        activeOpacity={0.5}>
                         <Avatar
                             rounded
                             source={{ uri: auth?.currentUser?.photoURL }}
                         />
                     </TouchableOpacity>
                 </View>
+            ),
+            headerRight: () => (
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    width: 80,
+                    marginRight: 20,
+                }}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <AntDesign name="camerao" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <SimpleLineIcons name="pencil" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
             )
         });
-    }, [])
+    }, [navigation])
 
     return (
         <SafeAreaView>
