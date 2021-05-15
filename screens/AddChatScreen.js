4cb00d30e0a347c3036d6e0dc3df0d9a3fc78889
mike <apolloproductionsdj@gmail.com>
2021-05-14 20:12:04 -0700

    create createChat function in order to add input into collection and navigate back to home page

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,17 +1,40 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { db } from '../firebase';
 
 const AddChatScreen = ({ navigation }) => {
+    const [input, setInput] = useState("");
+
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Add a new chat",
+            headerBackTitle: "Chats",
         })
     }, [navigation]);
 
+    const createChat = async () => {
+        await db.collection('chats').add({
+            chatName: input
+        }).then(() => {
+            navigation.goBack();
+        }).catch((error) => alert(error));
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Add Chat Screen</Text>
+            <Input
+                placeholder="Enter a chatname"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={createChat}
+                leftIcon={
+                    <Icon name="wechat" type="antdesign" size={24} color="black" />
+                }
+            />
+            <Button onPress={createChat} title="Create New Chat" />
+
         </View>
     )
 }
